b5f5e5b689a18e1672ff4f4445cb7bb29c40f664
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2018-09-19 10:19:49 +0100

    lambda for neurites

--- src/neurite_demo.h
+++ src/neurite_demo.h
@@ -21,15 +21,38 @@ namespace bdm {
 
 using namespace std;
 
+template <typename TSimulation = Simulation<>>
 struct NeuriteElongation : public BaseBiologyModule {
   NeuriteElongation() : BaseBiologyModule(gAllBmEvents) {}
 
   template <typename T>
   void Run(T* sim_object) {
-    auto&& neurite = sim_object->template ReinterpretCast<
-        experimental::neuroscience::NeuriteElement>();
+    auto* sim = TSimulation::GetActive();
+    auto&& ne = sim_object->template ReinterpretCast<experimental::neuroscience::NeuriteElement>();
 
-    neurite->ElongateTerminalEnd(10, {0, 0, 1});
+    int ownType = 0;
+    int otherType = 0;
+    auto countNeighbours = [&](auto&& neighbor, SoHandle neighbor_handle) {
+      // if neighbor is a NeuriteElement
+      if (neighbor->template IsSoType<experimental::neuroscience::NeuriteElement>()) {
+        auto&& neighbor_rc = neighbor->template
+          ReinterpretCast<experimental::neuroscience::NeuriteElement>();
+        auto n_soptr = neighbor_rc->GetSoPtr();
+        // if it is a direct relative
+        if (n_soptr->GetNeuronSomaOfNeurite() == ne->GetNeuronSomaOfNeurite()) {
+          ownType++;
+        }
+        else {
+          otherType++;
+        }
+      }
+    };
+
+    auto* grid = sim->GetGrid();
+    grid->ForEachNeighborWithinRadius(countNeighbours, ne, ne->GetSoHandle(), 4);
+    if (ownType >= otherType) {
+      ne->ElongateTerminalEnd(10, {0, 0, 1});
+    }
   }
 
   ClassDefNV(NeuriteElongation, 1);
@@ -38,7 +61,7 @@ struct NeuriteElongation : public BaseBiologyModule {
 // Define compile time parameter
 template <typename Backend>
 struct CompileTimeParam : public DefaultCompileTimeParam<Backend> {
-  using BiologyModules = Variant<NeuriteElongation>;
+  using BiologyModules = Variant<NeuriteElongation<>>;
   using AtomicTypes =
       VariadicTypedef<experimental::neuroscience::NeuronSoma,
                       experimental::neuroscience::NeuriteElement>;
@@ -67,7 +90,7 @@ static void CellCreator(double min, double max, int num_cells) {
     soma.SetDiameter(random->Uniform(7, 8));  // random diameter
 
     auto&& ne = soma.ExtendNewNeurite({0, 0, 1});
-    ne->AddBiologyModule(NeuriteElongation());
+    ne->AddBiologyModule(NeuriteElongation<>());
   }
   container->Commit();
 
@@ -81,7 +104,7 @@ inline int Simulate(int argc, const char** argv) {
 
   param->bound_space_ = true;
   param->min_bound_ = 0;
-  param->max_bound_ = 50;
+  param->max_bound_ = 150;
 
   CellCreator(param->min_bound_, param->max_bound_, 10);
 
