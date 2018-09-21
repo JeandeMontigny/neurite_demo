f0bc53906fcb3c5ff1095d9cbbf6d700157b461b
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2018-09-21 16:15:22 +0100

    WIP trying to create a soma

--- src/neurite_demo.h
+++ src/neurite_demo.h
@@ -22,49 +22,49 @@ namespace bdm {
 using namespace std;
 
 //template <typename TSimulation = Simulation<>>
-struct NeuriteElongation : public BaseBiologyModule {
-  NeuriteElongation() : BaseBiologyModule(gAllEventIds) {}
-
-  /// Default event constructor
-  template <typename TEvent, typename TBm>
-    NeuriteElongation(const TEvent& event, TBm* other, uint64_t new_oid = 0) {
-  }
-
-  template <typename TEvent, typename... TBms>
-  void EventHandler(const TEvent&, TBms*...) {}
-
-  template <typename T, typename TSimulation = Simulation<>>
-  void Run(T* sim_object) {
-    auto* sim = TSimulation::GetActive();
-    auto&& ne = sim_object->template ReinterpretCast<experimental::neuroscience::NeuriteElement>();
-
-    int ownType = 0;
-    int otherType = 0;
-    auto countNeighbours = [&](auto&& neighbor, SoHandle neighbor_handle) {
-      // if neighbor is a NeuriteElement
-      if (neighbor->template IsSoType<experimental::neuroscience::NeuriteElement>()) {
-        auto&& neighbor_rc = neighbor->template
-          ReinterpretCast<experimental::neuroscience::NeuriteElement>();
-        auto n_soptr = neighbor_rc->GetSoPtr();
-        // if it is a direct relative
-        if (n_soptr->GetNeuronSomaOfNeurite() == ne->GetNeuronSomaOfNeurite()) {
-          ownType++;
-        }
-        else {
-          otherType++;
-        }
-      }
-    };
-
-    auto* grid = sim->GetGrid();
-    grid->ForEachNeighborWithinRadius(countNeighbours, ne, ne->GetSoHandle(), 4);
-    if (ownType >= otherType) {
-      ne->ElongateTerminalEnd(10, {0, 0, 1});
-    }
-  }
-
-  ClassDefNV(NeuriteElongation, 1);
-};
+// struct NeuriteElongation : public BaseBiologyModule {
+//   NeuriteElongation() : BaseBiologyModule(gAllEventIds) {}
+//
+//   /// Default event constructor
+//   template <typename TEvent, typename TBm>
+//     NeuriteElongation(const TEvent& event, TBm* other, uint64_t new_oid = 0) {
+//   }
+//
+//   template <typename TEvent, typename... TBms>
+//   void EventHandler(const TEvent&, TBms*...) {}
+//
+//   template <typename T, typename TSimulation = Simulation<>>
+//   void Run(T* sim_object) {
+//     auto* sim = TSimulation::GetActive();
+//     auto&& ne = sim_object->template ReinterpretCast<experimental::neuroscience::NeuriteElement>();
+//
+//     int ownType = 0;
+//     int otherType = 0;
+//     auto countNeighbours = [&](auto&& neighbor, SoHandle neighbor_handle) {
+//       // if neighbor is a NeuriteElement
+//       if (neighbor->template IsSoType<experimental::neuroscience::NeuriteElement>()) {
+//         auto&& neighbor_rc = neighbor->template
+//           ReinterpretCast<experimental::neuroscience::NeuriteElement>();
+//         auto n_soptr = neighbor_rc->GetSoPtr();
+//         // if it is a direct relative
+//         if (n_soptr->GetNeuronSomaOfNeurite() == ne->GetNeuronSomaOfNeurite()) {
+//           ownType++;
+//         }
+//         else {
+//           otherType++;
+//         }
+//       }
+//     };
+//
+//     auto* grid = sim->GetGrid();
+//     grid->ForEachNeighborWithinRadius(countNeighbours, ne, ne->GetSoHandle(), 4);
+//     if (ownType >= otherType) {
+//       ne->ElongateTerminalEnd(10, {0, 0, 1});
+//     }
+//   }
+//
+//   ClassDefNV(NeuriteElongation, 1);
+// };
 
 // define compile time parameter
 BDM_CTPARAM(experimental::neuroscience) {
@@ -72,40 +72,40 @@ BDM_CTPARAM(experimental::neuroscience) {
 
   // using SimObjectTypes = CTList<NeuronSoma, NeuriteElement>;
   using NeuronSoma = experimental::neuroscience::NeuronSoma;
-  using NeuriteElement = experimental::neuroscience::NeuriteElement;
+  // using NeuriteElement = experimental::neuroscience::NeuriteElement;
 
-  BDM_CTPARAM_FOR(bdm, NeuriteElement) {
-    using BiologyModules =
-        CTList<NeuriteElongation>;
+  BDM_CTPARAM_FOR(experimental::neuroscience, NeuriteElement) {
+    // using BiologyModules =
+    //     CTList<NeuriteElongation>;
   };
 };
 
 // define my cell creator
-template <typename TSimulation = Simulation<>>
-static void CellCreator(double min, double max, int num_cells) {
-  auto* sim = TSimulation::GetActive();
-  auto* rm = sim->GetResourceManager();
-  auto* random = sim->GetRandom();
-
-  using Soma = experimental::neuroscience::NeuronSoma;
-  auto* container = rm->template Get<Soma>();
-  container->reserve(num_cells);
-
-  for (int i = 0; i < num_cells; i++) {
-    double x = random->Uniform(min, max);
-    double y = random->Uniform(min, max);
-    double z = random->Uniform(10);
-
-    std::array<double, 3> position = {x, y, z};
-    auto&& soma = rm->template New<Soma>(position);
-    soma.SetDiameter(random->Uniform(7, 8));  // random diameter
-
-    auto&& ne = soma.ExtendNewNeurite({0, 0, 1});
-    ne->AddBiologyModule(NeuriteElongation());
-  }
-  container->Commit();
-
-}  // end CellCreator
+// template <typename TSimulation = Simulation<>>
+// static void CellCreator(double min, double max, int num_cells) {
+//   auto* sim = TSimulation::GetActive();
+//   auto* rm = sim->GetResourceManager();
+//   auto* random = sim->GetRandom();
+//
+//   using Soma = experimental::neuroscience::NeuronSoma;
+//   auto* container = rm->template Get<Soma>();
+//   container->reserve(num_cells);
+//
+//   for (int i = 0; i < num_cells; i++) {
+//     double x = random->Uniform(min, max);
+//     double y = random->Uniform(min, max);
+//     double z = random->Uniform(10);
+//
+//     std::array<double, 3> position = {x, y, z};
+//     auto&& soma = rm->template New<Soma>(position);
+//     soma.SetDiameter(random->Uniform(7, 8));  // random diameter
+//
+//     auto&& ne = soma.ExtendNewNeurite({0, 0, 1});
+//     ne->AddBiologyModule(NeuriteElongation());
+//   }
+//   container->Commit();
+//
+// }  // end CellCreator
 
 template <typename TSimulation = Simulation<>>
 inline int Simulate(int argc, const char** argv) {
@@ -119,10 +119,21 @@ inline int Simulate(int argc, const char** argv) {
   Simulation<> simulation(argc, argv, set_param);
   auto* param = simulation.GetParam();
   auto* scheduler = simulation.GetScheduler();
+  auto* rm = simulation.GetResourceManager();
 
-  CellCreator(param->min_bound_, param->max_bound_, 10);
+//  CellCreator(param->min_bound_, param->max_bound_, 10);
 
-  scheduler->Simulate(100);
+  // auto* container = rm->template Get<experimental::neuroscience::NeuronSoma>();
+  // container->reserve(1);
+  // auto&& cell = rm->template
+  //  New<experimental::neuroscience::NeuronSoma>({0,0,0});
+  // container->Commit();
+
+  experimental::neuroscience::NeuronSoma soma({0,0,0});
+  // auto&& ne = soma.ExtendNewNeurite({0, 0, 1});
+  // ne->AddBiologyModule(NeuriteElongation());
+
+  simulation.GetScheduler()->Simulate(100);
 
   return 0;
 }
